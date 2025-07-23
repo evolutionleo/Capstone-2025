64a14c07d440c2d532798ef13ba77bf781e8a8f6
fantom73d <psdimakorletyanu@gmail.com>
2025-07-24 02:19:48 +0300

    L15 added

--- LightLethargy/Assets/Scripts/Objects/DialogObject.cs
+++ LightLethargy/Assets/Scripts/Objects/DialogObject.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Events;
