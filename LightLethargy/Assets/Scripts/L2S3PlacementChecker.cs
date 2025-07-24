fb5f96e2dd27db5fe430fb9169761f2e61fd9bec
fantom73d <psdimakorletyanu@gmail.com>
2025-07-24 06:39:12 +0300

    Complete level 2 screen 3

---
+++ LightLethargy/Assets/Scripts/L2S3PlacementChecker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Objects;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class L2S3PlacementChecker : MonoBehaviour
+{
+    [SerializeField] private BulbPlace[] places;
+    [SerializeField] private BulbPlace[] correctPlaces;
+    [SerializeField] private UnityEvent onCorrectPlacement;
+    [SerializeField] private UnityEvent onWrongPlacement;
+
+    private void Awake()
+    {
+        foreach (var place in places)
+        {
+            place.ChangedBulb += CheckPlacement;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var place in places)
+        {
+            place.ChangedBulb -= CheckPlacement;
+        }
+    }
+
+    private void CheckPlacement(bool _)
+    {
+        var filledPlaces = 0;
+        foreach (var place in places)
+        {
+            if (place.HasBulb)
+            {
+                filledPlaces++;
+            }
+        }
+
+        if (filledPlaces < correctPlaces.Length)
+        {
+            // Placement is not complete, yet
+            return;
+        }
+
+        foreach (var place in correctPlaces)
+        {
+            if (!place.HasBulb)
+            {
+                onWrongPlacement.Invoke();
+                return;
+            }
+        }
+        
+        onCorrectPlacement.Invoke();
+    }
+}
